a57fc391046a9940a02d689b1dd33ae215156ae4
alexlam184 <alexwork0211@gmai.com>
2024-11-22 16:44:42 -0800

    change model

--- src/components/Live2DField.tsx
+++ src/components/Live2DField.tsx
@@ -46,12 +46,12 @@ function Live2DField(props: Live2DFieldProps) {
     app.renderer.plugins.interaction.destroy(); // Remove the default interaction manager
 
     const loadModels = async () => {
-      // model = await Live2DModel.from(
-      //   "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json"
-      // );
       model = await Live2DModel.from(
-        "https://raw.githubusercontent.com/alexlam184/emla-chat-web/master/src/assets/model/%E6%9C%AA%E5%91%BD%E5%90%8D/%E6%9C%AA%E5%91%BD%E5%90%8D.model3.json"
+        "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json"
       );
+      // model = await Live2DModel.from(
+      //   "https://raw.githubusercontent.com/alexlam184/emla-chat-web/master/src/assets/model/%E6%9C%AA%E5%91%BD%E5%90%8D/%E6%9C%AA%E5%91%BD%E5%90%8D.model3.json"
+      // );
       app.stage.addChild(model);
 
       model.scale.set(0.5);
